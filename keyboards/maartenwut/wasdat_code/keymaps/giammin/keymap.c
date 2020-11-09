a3a11a881d12c2aabf644414e531cd5ebd9756d0
giammin <giammin@gmail.com>
2020-11-09 22:21:34 +0100

    layer1

--- keyboards/maartenwut/wasdat_code/keymaps/giammin/keymap.c
+++ keyboards/maartenwut/wasdat_code/keymaps/giammin/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│GUI │Alt │                        │ Alt│ GUI│Menu│Ctrl│ │ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
      */
-    LAYOUT_fullsize_ansi(
+    LAYOUT_tkl_ansi(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,      KC_PSCR, KC_SLCK, KC_PAUS,
 
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,
@@ -41,12 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,              KC_UP,
         KC_LALT, KC_LGUI, KC_LCTL,                            KC_SPC,                             KC_RCTL, KC_RGUI, MO(1),   KC_RALT,     KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    LAYOUT_fullsize_ansi(
+    LAYOUT_tkl_ansi(
         RESET,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_STEP, BL_TOGG,    _______,  _______, KC_MUTE,
 
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_MPLY,  KC_MSTP, KC_VOLU,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_MPRV,  KC_MNXT, KC_VOLD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,     KC_MPLY,  KC_MSTP, KC_VOLU,
+        KC_CAPS, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_INS,  KC_PSCR, _______, _______,    KC_MPRV,  KC_MNXT, KC_VOLD,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_DEL,           _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,              _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______,    _______,  _______, _______
     )
